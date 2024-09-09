e8811dc521e31bd9b4cb0fed2653f13ada681540
FindMalek <malekgarahellalbus@gmail.com>
2024-09-09 19:31:52 +0100

    fix: resolving 'body' in '/api/token'

--- app/api/token/route.ts
+++ app/api/token/route.ts
@@ -6,7 +6,6 @@ import { db } from "@/lib/prisma"
 
 export async function GET(request: NextRequest) {
   const headersList = headers()
-  const body = await request.json()
 
   const req_token = headersList.get("x-api-key") as string
   const api_token = await db.aPIToken.findFirst({
