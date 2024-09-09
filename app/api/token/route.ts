6ba3cde58c1a5cc5ec83d28548db09f90fd0a515
findmalek <hi@findmalek.com>
2024-09-09 12:16:02 +0100

    feat: added 'token' endpoint

---
+++ app/api/token/route.ts
@@ -0,0 +1,26 @@
+import { headers } from "next/headers"
+import { NextRequest, NextResponse } from "next/server"
+
+import { returnError } from "@/lib/api"
+import { db } from "@/lib/prisma"
+
+export async function GET(request: NextRequest) {
+  const headersList = headers()
+  const req_token = headersList.get("x-api-key") as string
+    
+  const api_token = await db.aPIToken.findFirst({
+    where: {
+      id: req_token,
+      deletedAt: null,
+    },
+  })
+
+  if (!api_token) {
+    return returnError({
+      error: "ERROR: Invalid API token.",
+      status: 401,
+    })
+  }
+
+  return NextResponse.json({ token: api_token.token })
+}
