8cf15d42f77833606f55e416d6cda005cea21de6
findmalek <hi@findmalek.com>
2024-10-11 14:25:09 +0100

    fix: resolved miss-type of env

--- lib/undrstnd.ts
+++ lib/undrstnd.ts
@@ -5,12 +5,12 @@ import { env } from "@/env.mjs"
 
 const demo_client = createOpenAI({
   apiKey: env.GROQ_API_KEY,
-  baseURL: env.GR_LPU_ENDPOINT,
+  baseURL: env.GROQ_API_ENDPOINT,
 })
 
 const undrstnd_client = (api_key: string) => {
   return createOpenAI({
-    baseURL: env.GR_LPU_ENDPOINT,
+    baseURL: env.GROQ_API_ENDPOINT,
     apiKey: api_key,
   })
 }
